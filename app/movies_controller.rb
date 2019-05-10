408a3212b0f9012ebf7b62fcf5b1173c35627bd4
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 03:04:30 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -33,6 +33,7 @@ def can_be_created_in_a_block(args = {:title => "Home Alone", :release_date => 1
   # release_date == 1990
   
   Movie.create do |m|
+    m.title = args[:title]
     binding.pry
   end
 end
