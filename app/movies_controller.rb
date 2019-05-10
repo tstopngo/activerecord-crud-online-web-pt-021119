c3192d21d7a92810d2171e89333b978d848313c5
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 03:36:49 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -94,7 +94,7 @@ def can_update_multiple_items_at_once
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
   Movie.all.each do |movie|
-    movie.update (title: "A Movie")
+    movie.update(title: "A Movie")
   end
 end
 
