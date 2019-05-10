42f82b294b4b44f8bac78ef3516451efb6d124b1
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 03:33:59 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -83,10 +83,9 @@ end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  movie = Movie.create(title: "Wat?")
+  Movie.create(title: "Wat?")
   movie = Movie.find_by(title:"Wat?")
   movie.update(title:"Wat,huh?")
-  movie
 end
 
 def can_update_multiple_items_at_once
