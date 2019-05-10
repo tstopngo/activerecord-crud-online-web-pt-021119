d73264ae1076ed9959f0de1aa2005feae9e46333
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 03:06:55 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -39,7 +39,6 @@ def can_be_created_in_a_block(args = {:title => "Home Alone", :release_date => 1
     m.director = args[:director]
     m.lead = args[:lead]
     m.in_theaters = args[:in_theaters]
-    m
   end
 end
 
