ad1d4e88d9ba38ec7b9f8dafd7f1473cb06af36a
Tracy Ngo <tngo1206@gmail.com>
2019-05-10 02:41:49 +0000

    Automatically backed up by Learn

--- db/migrate/20190510024043_create_movies.rb
+++ db/migrate/20190510024043_create_movies.rb
@@ -3,6 +3,7 @@ class CreateMovies < ActiveRecord::Migration[5.2]
     create_table :movies do |t|
       t.string :title
       t.integer :release_date
+      t.string :director
     end
   end
 end
